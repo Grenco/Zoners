f73b5d6dfdf613f7377833496eeafaae309ba65b
Graeme Smyrl <graemesmyrl@gmail.com>
2021-04-11 19:59:42 +0100

    Added powerup preset and class

---
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        MultiplayerControls player = collision.gameObject.GetComponent<MultiplayerControls>();
+        ApplyPowerUp(player);
+    }
+
+    protected void ApplyPowerUp(MultiplayerControls player)
+    {
+        player.DisableControls();
+        gameObject.SetActive(false);
+    }
+}
